1eb818a9597c7d615278c6487bc983c0df7e61b4
Prathmesh Takalkar <takalkarprathmesh@gmail.com>
2023-03-08 09:18:20 +0530

    first commit

---
+++ src/emojibase.js
@@ -0,0 +1,134 @@
+const emojibase = [
+    {
+        id: 1,
+        emoji: "🤭",
+        name: "Face with Hand Over Mouth",
+        meaning:
+          "A yellow face with a hand covering its mouth. Displays most often with smiling eyes and/or blushing cheeks, suggesting coy laughter or embarrassment, as if cheekily saying Oops!"
+    },
+    {
+        id: 2,
+        emoji: "😋",
+        name: "Face Savoring Food",
+        meaning:
+          "A yellow face with smiling eyes and a broad, closed smile with its tongue sticking out of one corner, as if licking its lips in appetite or contentment. Widely used to convey that a food item is delicious. May also express that a person is attractive."
+    },
+    {
+        id: 3,
+        emoji: "🥵",
+        name: "Hot Face",
+        meaning:
+          "A reddish-orange face with furrowed eyebrows, tongue stuck out, and beads of sweat, as if overheated from high temperatures. Number and position of sweat droplets vary by platforms. May also represent physical attractiveness (slang, hot)."
+    },
+      
+    {
+      id: 4,
+      emoji: "😀",
+      name: "Grinning Face",
+      meaning:
+        "A yellow face with simple, open eyes and a broad, open smile, showing upper teeth and tongue on some platforms. Often conveys general pleasure and good cheer or humor."
+    },
+    {
+      id: 5,
+      emoji: "🙂",
+      name: "Slightly Smiling Face",
+      meaning:
+        "A yellow face with simple, open eyes and a thin, closed smile. Conveys a wide range of positive, happy, and friendly sentiments. Its tone can also be patronizing, passive-aggressive, or ironic, as if saying This is fine when it’s really not."
+    },
+    {
+      id: 6,
+      emoji: "😙",
+      name: "Kissing Face with Smiling Eyes",
+      meaning:
+        "A yellow face with smiling eyes and puckered lips giving a kiss. Commonly conveys sentiments of love and affection."
+    },
+    {
+    id: 7,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+    },
+    {
+    id: 8,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+    },
+    {
+    id: 9,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+    },
+    {
+      id: 10,
+      emoji: "🤭",
+      name: "Face with Hand Over Mouth",
+      meaning:
+        "A yellow face with a hand covering its mouth. Displays most often with smiling eyes and/or blushing cheeks, suggesting coy laughter or embarrassment, as if cheekily saying Oops!"
+  },
+  {
+      id: 11,
+      emoji: "😋",
+      name: "Face Savoring Food",
+      meaning:
+        "A yellow face with smiling eyes and a broad, closed smile with its tongue sticking out of one corner, as if licking its lips in appetite or contentment. Widely used to convey that a food item is delicious. May also express that a person is attractive."
+  },
+  {
+      id: 12,
+      emoji: "🥵",
+      name: "Hot Face",
+      meaning:
+        "A reddish-orange face with furrowed eyebrows, tongue stuck out, and beads of sweat, as if overheated from high temperatures. Number and position of sweat droplets vary by platforms. May also represent physical attractiveness (slang, hot)."
+  },
+    
+  {
+    id: 13,
+    emoji: "😀",
+    name: "Grinning Face",
+    meaning:
+      "A yellow face with simple, open eyes and a broad, open smile, showing upper teeth and tongue on some platforms. Often conveys general pleasure and good cheer or humor."
+  },
+  {
+    id: 14,
+    emoji: "🙂",
+    name: "Slightly Smiling Face",
+    meaning:
+      "A yellow face with simple, open eyes and a thin, closed smile. Conveys a wide range of positive, happy, and friendly sentiments. Its tone can also be patronizing, passive-aggressive, or ironic, as if saying This is fine when it’s really not."
+  },
+  {
+    id: 15,
+    emoji: "😙",
+    name: "Kissing Face with Smiling Eyes",
+    meaning:
+      "A yellow face with smiling eyes and puckered lips giving a kiss. Commonly conveys sentiments of love and affection."
+  },
+  {
+  id: 16,
+  emoji: "💪",
+  name: "Tense Biceps",
+  meaning:
+    "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+  id: 17,
+  emoji: "🙏",
+  name: "Person With Folded Hands",
+  meaning:
+    "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+  id:18,
+  emoji: "🤣",
+  name: "Rolling On The Floor, Laughing",
+  meaning:
+    "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  }
+  ];
+
+  
+  export default emojibase;
+  
